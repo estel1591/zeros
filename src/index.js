7ad563c27910d9c7c72371ac59ced669e51695df
estel1591 <estel1591@mail.ru>
2018-09-30 12:58:37 +0300

    update index.js (correct function getZerosCount)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let check = true;
+    let exponent = 0;
+    
+    while (check) {
+        check = false;
+        if (number/Math.pow(5, exponent) > 1) {
+            check = true;
+            exponent++;
+        }
+    }   
+    
+    let zerosCount = 0;
+    for (let i = 1; i < exponent; i++){
+        zerosCount = zerosCount + Math.trunc( number/Math.pow(5, i) );
+    }
+    
+    return zerosCount;
 }
